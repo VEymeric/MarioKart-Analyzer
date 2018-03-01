afebda4ddef9cc2dc56f9c30566de62ddbe7be64
VEymeric <eymeric.vandaele@isen-lille.fr>
2018-03-01 20:38:30 +0100

    detection level faster than nether

--- programs/check_map.py
+++ programs/check_map.py
@@ -7,36 +7,22 @@ from scipy.linalg import norm
 from scipy import sum, average
 
 
-def get_grey_images_from_dir(directory):
-    files = glob.glob(directory + "\*")
-    imgs_grey = []
-    for file in files:
-        imgs_grey.append(to_grayscale(imread(file).astype(float)))
-    return imgs_grey
-
-
-def get_grey_images_from_file(file):
-    return to_grayscale(imread(file).astype(float))
-
-
-
-def check_map(arg1, arg2):
+def main():
     """
     This function compare the full image with the database of maps and return te name of the level
-    where the Manhattan norm is the lowest
+    where the MAnhattan norm is the lowest
     2 arguments :
         The path to the directory with all your maps screen
         The image you wanna check
     """
-    directory_map = arg1
-    image_test = arg2
+    directory_map = sys.argv[1]
+    image_test = sys.argv[2]
     image_test = to_grayscale(imread(image_test).astype(float))
     files = glob.glob(directory_map +"\*")
     # read images as 2D arrays (convert to grayscale for simplicity)
     imgs = []
     for file in files:
         imgs.append(to_grayscale(imread(file).astype(float)))
-    print(imgs)
 
     # compare
     index = 0
@@ -53,67 +39,28 @@ def check_map(arg1, arg2):
         index_c += 1
     print("=> ", os.path.basename(files[index])[:-4])
 
-
-def check_state(directory_state, image_test):
-    """
-    This function compare the full image with the database of state and return the name of the stat
-    where the Manhattan norm if he is < 15.
-    2 arguments :
-        The path to the directory with all your state screen
-        The image you wanna check
-    """
-    image_test = to_grayscale(imread(image_test).astype(float))
-    files = glob.glob(directory_state +"\*")
-    # read images as 2D arrays (convert to grayscale for simplicity)
-    imgs = []
-    for file in files:
-        imgs.append(to_grayscale(imread(file).astype(float)))
-
-    # compare
-    index = 0
-    index_c = 0
-    min = 100
-    for img in imgs:
-        n_m, n_0 = compare_images(image_test, img)
-        print(os.path.basename(files[index_c])[:-4])
-        print("Manhattan norm:", n_m, "/ per pixel:", n_m/img.size)
-        if(n_m/img.size < min):
-            index = index_c
-            min = n_m/img.size
-        index_c += 1
-    print("=> ", os.path.basename(files[index])[:-4], " : ", min)
-    if(min <15):
-        return os.path.basename(files[index])[:-4]
-    return ""
-
-
-def compare_images_value(img1, img2):
+def compare_images(img1, img2):
     # normalize to compensate for exposure difference, this may be unnecessary
     # consider disabling it
-    # img1 = normalize(img1)
-    # img2 = normalize(img2)
+    img1 = normalize(img1)
+    img2 = normalize(img2)
     # calculate the difference and its norms
     diff = img1 - img2  # elementwise for scipy arrays
     m_norm = sum(abs(diff))  # Manhattan norm
-    return m_norm
+    z_norm = norm(diff.ravel(), 0)  # Zero norm
+    return (m_norm, z_norm)
 
-def is_same_image(img1, img2, score):
-    return score <= compare_images_value(img1, img2)
-
-
-def to_grayscale(array):
+def to_grayscale(arr):
     "If arr is a color image (3D array), convert it to grayscale (2D array)."
-    if len(array.shape) == 3:
-        return average(array, -1)  # average over the last axis (color channels)
+    if len(arr.shape) == 3:
+        return average(arr, -1)  # average over the last axis (color channels)
     else:
-        return array
+        return arr
 
-
-def normalize(array):
-    rng = array.max()-array.min()
-    amin = array.min()
-    return (array-amin)*255/rng
-
+def normalize(arr):
+    rng = arr.max()-arr.min()
+    amin = arr.min()
+    return (arr-amin)*255/rng
 
 if __name__ == "__main__":
-    check_map(sys.argv[1],sys.argv[2])
+    main()
--- programs/images_functions.py
+++ programs/images_functions.py
@@ -1,5 +1,6 @@
 import glob
 import os.path
+import cv2
 from scipy.misc import imread
 from scipy import sum, average
 
@@ -13,7 +14,7 @@ def get_grey_images_from_dir(directory):
     imgs_grey = []
     for file in files:
         imgs_grey.append(to_grayscale(imread(file).astype(float)))
-    return imgs_grey
+    return files, imgs_grey
 
 
 def get_grey_images_from_file(file):
@@ -23,20 +24,33 @@ def get_grey_images_from_file(file):
 def compare_images_value(img1, img2):
     # normalize to compensate for exposure difference, this may be unnecessary
     # consider disabling it
-    # img1 = normalize(img1)
-    # img2 = normalize(img2)
+    #img1 = normalize(img1)
+    #img2 = normalize(img2)
     # calculate the difference and its norms
     diff = img1 - img2  # elementwise for scipy arrays
     m_norm = sum(abs(diff))  # Manhattan norm
-    return m_norm
+    return m_norm/img1.size
+
+
+def score_compare_image_and_folder(folder, image):
+    files, imgs_grey = get_grey_images_from_dir(folder)
+    best_index = None
+    best_score = 100000000000000
+    for index, file in enumerate(imgs_grey):
+        score = compare_images_value(image, file)
+        #print(score, name_of_image(files[index]))
+        if score < best_score:
+            best_score = score
+            best_index = index
+    return best_score, name_of_image(files[best_index])
 
 
 def is_same_image(img1, img2, score):
-    return score <= compare_images_value(img1, img2)
+    return score > compare_images_value(img1, img2)
 
 
 def to_grayscale(array):
-    "If arr is a color image (3D array), convert it to grayscale (2D array)."
+    """If arr is a color image (3D array), convert it to grayscale (2D array)."""
     if len(array.shape) == 3:
         return average(array, -1)  # average over the last axis (color channels)
     else:
@@ -47,3 +61,8 @@ def normalize(array):
     rng = array.max()-array.min()
     amin = array.min()
     return (array-amin)*255/rng
+
+
+def save_image_gray(frame, name, folder):
+    #frame = to_grayscale(imread(frame).astype(float))
+    cv2.imwrite(folder + "/" + name + ".jpg", frame)  # save frame as JPEG file
